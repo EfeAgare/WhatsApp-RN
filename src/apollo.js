440aca53dd69e18e2815c2cd54c00d516080a1eb
Efe <knowledgeagare157@gmail.com>
2020-07-22 18:14:07 +0100

    setup apollo client

--- src/apollo.js
+++ src/apollo.js
@@ -5,59 +5,45 @@ import { WebSocketLink } from 'apollo-link-ws';
 import { split, ApolloLink } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
 import { createUploadLink } from 'apollo-upload-client';
-import * as Keychain from 'react-native-keychain';
 
-const makeApolloClient = async () => {
-  // Create an http link:
+const makeApolloClient = () => {
+  // create an apollo link instance, a network interface for apollo client
   const httpLink = new createUploadLink({
     uri: 'http://localhost:4000/graphql',
   });
-  
-    // Retrieve the credentials
-   let credentials = await Keychain.getGenericPassword();
-  
 
-  const middlewareLink = setContext((req, previousContext) => {
-    if (credentials) {
-      return {
-        headers: {
-          'x-token': `${credentials?.token}`,
-        },
-      };
-    }
+  // console.log('credentials hmmm', credentials);
+  // Keychain.getGenericPassword()
+  //   .then((data) => {
+  //     credentials = data;
 
-    return previousContext;
-  });
+  //     console.log('credentials ok m', credentials);
+  //     return credentials;
+  //   })
+  //   .catch((error) => {
+  //     console.log("Keychain couldn't be accessed! Maybe no value set?", error);
+  //   });
 
-  const afterwareLink = new ApolloLink(async (operation, forward) => {
-    const { headers } = operation.getContext();
-
-    if (headers) {
-      const token = headers.get('x-token');
-
-      if (token) {
-        // Store the credentials
-        await Keychain.setGenericPassword(token);
-      }
-    }
-
-    return forward(operation);
-  });
-
-  const httpLinkWithMiddleware = afterwareLink.concat(
-    middlewareLink.concat(httpLink)
-  );
-
+  // console.log('credentials hmmm', credentials);
   // Create a WebSocket link:
   const wsLink = new WebSocketLink({
     uri: `ws://localhost:4000/graphql`,
     options: {
       reconnect: true,
-      connectionParams: {
-        token: credentials?.token,
-      },
+      // connectionParams: {
+      //   token: credentials?.password,
+      // },
     },
   });
+
+  const middlewareLink = setContext(() => ({
+    headers: {
+      'x-token': `${credentials?.password}`,
+    },
+  }));
+
+  // const httpLinkWithMiddleware = middlewareLink.concat(httpLink);
+
   // using the ability to split links, you can send data to each link
   // depending on what kind of operation is being sent
   const link = split(
@@ -70,14 +56,17 @@ const makeApolloClient = async () => {
       );
     },
     wsLink,
-    httpLinkWithMiddleware
+    httpLink
   );
+
+  // create an inmemory cache instance for caching graphql data
+  const cache = new InMemoryCache();
+
   // instantiate apollo client with apollo link instance and cache instance
   const client = new ApolloClient({
     link,
-    cache: new InMemoryCache(),
+    cache,
   });
   return client;
 };
-
 export default makeApolloClient;
